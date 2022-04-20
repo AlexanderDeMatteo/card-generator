65cdd17a022f6f55a0d5f06e8f2a4b86d7c0e2e7
alexander de matteo1~ <aldml0201@gmail.com1~>
2022-04-20 11:23:02 -0400

    card generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// import "bootstrap";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+// window.onload = function() {
+//   //write your code here
+//   console.log("Hello Rigo from the console!");
+// };
+// import "./style.css";
+
+
+window.onload = () => {
+   document.querySelector('.card').classList.add(generateRandomSuit());
+   document.querySelector('.card').innerHTML = (generateRandomNumber());
+   setTimeout(() =>{
+     document.querySelector('.card').classList.remove('heart');
+     document.querySelector('.card').classList.add('spade');
+   }, 1000);
+};
+
+let generateRandomNumber = () => {
+  let numbers = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+  let indexNumber = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumber]
+
+  
+};
+let generateRandomSuit = () => {
+  let suit = ["diamond", "spade","heart","club"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit]
 };
